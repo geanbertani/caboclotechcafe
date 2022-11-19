16506612ca8f490cd09edfd79cfcfe9370d1566b
Gean Bertani <geanbertani@gmail.com>
2022-11-18 21:12:11 -0400

    ajustes na página pedido

--- core/theme/pedido.php
+++ core/theme/pedido.php
@@ -1,1 +1,14 @@
-<h1>Pedido</h1>
+<section class="products">
+    <h1 class="titleSection">Pedido</h1>
+    <div class="product uk-flex uk-flex-between uk-margin-bottom">
+        <div class="productImage uk-flex uk-flex-middle">
+            <img src="uploads/products/image/produto_foto_1.png" alt="Café com leite 180ml">
+        </div>
+        <div class="productBoxInfo uk-flex uk-flex-middle uk-flex-wrap">
+            <div class="productName">Café com leite 180ml</div>
+            <div class="productPrice">R$ 3,00</div>
+            <div class="productAdd uk-flex uk-flex-center"><input type="number" value="1" class="uk-input"></div>
+        </div>
+    </div>
+    <div class="productAdd uk-margin-bottom"><a class="uk-button" href="index.php?pag=sucesso">Fechar pedido</a></div>
+</section>
