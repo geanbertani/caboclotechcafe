22e8cd185db9cf8b2a119c54666aea0dffca3a7f
Gean Bertani <geanbertani@gmail.com>
2022-11-18 20:48:45 -0400

    ajustado os links de navegação

--- core/theme/header.php
+++ core/theme/header.php
@@ -5,13 +5,13 @@
         </a>
     </div>
     <div class="headerLogo uk-flex uk-flex-center">
-        <a href="#">
+        <a href="index.php">
             <img src="assets/image/logomarca.png" alt="Caboclo Tech Café">
         </a>
     </div>
     <div class="uk-flex uk-flex-middle">
-        <a href="#">
-            <img src="assets/image/ico-user.svg" alt="Login">
+        <a href="index.php?pag=pedido">
+            <img src="assets/image/ico-shopping-cart.svg" alt="Login">
         </a>
     </div>
 </header>
